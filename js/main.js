46d2f78e874cc624ebafeaf46c57bfba1ce56f25
Nastiia Zhyrnova <74502108+anastasiiazhyrnova@users.noreply.github.com>
2022-11-22 21:07:24 +0200

    Added functions for sorting table

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 (function ($) {
     "use strict";
     
-    // Dropdown on mouse hover
     $(document).ready(function () {
         function toggleNavbarMethod() {
             if ($(window).width() > 992) {
@@ -18,8 +17,6 @@
         $(window).resize(toggleNavbarMethod);
     });
 
-
-    // Date and time picker
     $('.date').datetimepicker({
         format: 'L'
     });
@@ -42,8 +39,8 @@
     });
 
 
-    // Doctor carousel
-    $(".doctor-carousel").owlCarousel({
+    // Price carousel
+    $(".price-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
         margin: 45,
@@ -67,7 +64,6 @@
         }
     });
 
-
     // Team carousel
     $(".team-carousel, .related-carousel").owlCarousel({
         autoplay: true,
@@ -90,8 +86,6 @@
         }
     });
 
-
-    // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
@@ -102,3 +96,131 @@
     
 })(jQuery);
 
+
+// sort func
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("le-Table-1");
+    switching = true;
+    dir = "asc";
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount++;
+        } else {
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}
+
+
+function filterTable(event) {
+    var filter = event.target.value.toUpperCase();
+    var rows = document.querySelector("#le-Table-1 tbody").rows;
+
+    for (var i = 0; i < rows.length; i++) {
+        var firstCol = rows[i].cells[0].textContent.toUpperCase();
+        var secondCol = rows[i].cells[1].textContent.toUpperCase();
+        var thirdCol = rows[i].cells[2].textContent.toUpperCase();
+        var sixthCol = rows[i].cells[5].textContent.toUpperCase();
+        if (firstCol.indexOf(filter) > -1 || secondCol.indexOf(filter) > -1 || thirdCol.indexOf(filter) > -1 || sixthCol.indexOf(filter) > -1) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }
+    }
+}
+document.querySelector('#le-Input-1').addEventListener('keyup', filterTable, false);
+
+function content(elem) {
+}
+
+
+// checkbox 
+function filter_type(box) {
+    var cbs = document.getElementsByTagName('input');
+    var all_checked_types = [];
+    for (var i = 0; i < cbs.length; i++) {
+        if (cbs[i].type == "checkbox") {
+            if (cbs[i].name.match(/^filter/)) {
+                if (cbs[i].checked) {
+                    all_checked_types.push(cbs[i].value);
+                }
+            }
+        }
+    }
+    if (all_checked_types.length > 0) {
+        $('#le-Table-1 tr').each(function (i, row) {
+            var $tds = $(this).find('td')
+            if ($tds.length) {
+                var type = $tds[2].innerText;
+                console.log(type)
+                if (!(type && all_checked_types.indexOf(type) >= 0)) {
+                    $(this).hide();
+                }
+                else {
+                    $(this).show();
+                }
+            }
+        });
+
+    }
+    else {
+        $('#le-Table-1 tr').each(function (i, row) {
+            var $tds = $(this).find('td'),
+                type = $tds.eq(2).text();
+            $(this).show();
+        });
+    }
+    return true;
+}
+
+jQuery(document).ready(function ($) {
+    $(".clickable-row").click(function () {
+        window.location = $(this).data("href");
+    });
+});
+
+
+// search input text
+function tableSearch() {
+    var phrase = document.getElementById('le-Input-1');
+    var table = document.getElementById('le-Table-1');
+    var regPhrase = new RegExp(phrase.value, 'i');
+    var flag = false;
+    for (var i = 1; i < table.rows.length; i++) {
+        flag = false;
+        for (var j = table.rows[i].cells.length - 1; j >= 0; j--) {
+            flag = regPhrase.test(table.rows[i].cells[j].innerHTML);
+            if (flag) break;
+        }
+        if (flag) {
+            table.rows[i].style.display = "";
+        } else {
+            table.rows[i].style.display = "none";
+        }
+
+    }
+}
